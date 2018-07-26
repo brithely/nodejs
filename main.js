df8f892b87cf46a7d53e09a366785c17382a8832
brithely <37111838+brithely@users.noreply.github.com>
2018-07-27 00:32:54 +0900

    0.75

--- main.js
+++ main.js
@@ -32,37 +32,16 @@ var app = http.createServer(function(request,response){
         });
       }
       else {
-        /*
-        fs.readdir('./data/', function(error, filelist){
-          var filteredID = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredID}`, 'utf8', function(err, description){
-            var list = templet.list(filelist);
-            var title = queryData.id;
-            var sanitizedTitle = sanitizeHtml(title);
-            var sanitizedDescription = sanitizeHtml(description, {allowedTags:['h1']});
-            var html = templet.html(sanitizedTitle, list, `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-            `<a href="/create">create</a>
-            <a href="/update?id=${sanitizedTitle}">update</a>
-            <form action="delete_process" method='post'>
-              <input type='hidden' name='id' value=${sanitizedTitle}>
-              <input type='submit' value='delete'>
-            </form>
-              `
-            );
-            response.writeHead(200);
-            response.end(html);
-          });
-        });
-      }*/
       db.query(`SELECT * from topic`, function(error, topics){
         if(error){
           throw error
         }
-        db.query(`SELECT * from topic where id=?`,[queryData.id], function(error2, topic){
+        db.query(`SELECT * from topic LEFT JOIN author ON topic.author_id = author.id where topic.id=?`,[queryData.id], function(error2, topic){
           var title = topic[0].title;
           var description = topic[0].description;
           var list = templet.list(topics);
-          var html = templet.html(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>
+          var html = templet.html(title, list, `<h2>${title}</h2>${description}
+            <p>by ${topic[0].name}</p>`, `<a href="/create">create</a>
           <a href="/update?id=${queryData.id}">update</a>
                       <form action="delete_process" method='post'>
                         <input type='hidden' name='id' value=${queryData.id}>
@@ -71,48 +50,32 @@ var app = http.createServer(function(request,response){
           response.writeHead(200);
           response.end(html);
         });
-
       });
     }
   }
     else if (pathName === '/create'){
-      /*
-      fs.readdir('./data/', function(error, filelist){
-        var title = 'Welcome';
-        var description = 'Hello, Node.js';
-        var list = templet.list(filelist);
-        var html = templet.html(title, list, `
-          <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder = "description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-          </form>
-          `, '');
-        response.writeHead(200);
-        response.end(html);
-        });
-        */
         db.query(`SELECT * from topic`, function(error, topics){
-          var title = 'Create';
-          var list = templet.list(topics);
-          var html = templet.html(title, list, `
-
+          db.query(`SELECT * FROM author`,function(error2,authors){
+            var title = 'Create';
+            var list = templet.list(topics);
+            var html = templet.html(title, list, `
             <form action="/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder = "description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-          </form>
-          `, `<a href="/create">create</a>`);
-          response.writeHead(200);
-          response.end(html);
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder = "description"></textarea>
+            </p>
+            <p>
+            ${templet.authorSelect(authors)}
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+            </form>
+            `, `<a href="/create">create</a>`);
+            response.writeHead(200);
+            response.end(html);
+          });
+
         });
 
     } else if (pathName === '/create_process'){
@@ -120,36 +83,52 @@ var app = http.createServer(function(request,response){
         request.on('data', function(data){
             body += data;
         });
-        request.on('end', function(){
-            var post = qs.parse(body);
-            var title = post.title;
-            var description = post.description;
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
+        request.on('data', function(data){
+        var post = qs.parse(body);
+        db.query(`
+          INSERT INTO topic (title, description, created, author_id)
+          VALUES (?, ?, NOW(),?)`, [post.title, post.description, post.author], function(error, result){
+            console.log(post.title);
+            if(error){
+              throw error;
+            }
+            response.writeHead(302, {Location: `/?id=${result.insertId}`});
+            response.end();
+          });
         });
     } else if(pathName === '/update'){
-      fs.readdir('./data/', function(error, filelist){
-      var filteredID = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredID}`, 'utf8', function(err, description){
-          var list = templet.list(filelist);
-          var title = queryData.id;
-          var html = templet.html(title, list, `
+      db.query(`SELECT * from topic`, function(error, topics){
+        if(error){
+          throw error;
+        }
+        db.query(`SELECT * from topic where id=?`,[queryData.id], function(error2, topic){
+          if(error2){
+            throw error2;
+          }
+          db.query(`SELECT * from author`, function(error3, authors){
+            var title = topic[0].title;
+            var description = topic[0].description;
+            var list = templet.list(topics);
+            var html = templet.html(title, list, `
             <form action="/update_process" method="post">
-            <input type="hidden" name ="id" value="${title}"
+            <input type="hidden" name ="id" value="${queryData.id}"
             <p><input type="text" name="title" placeholder="title" value="${title}"></p>
             <p>
               <textarea name="description" placeholder = "description">${description}</textarea>
             </p>
             <p>
+            ${templet.authorSelect(authors, topic[0].author_id)}
+            </p>
+            <p>
               <input type="submit">
             </p>
             </form>
-            `, '');
-          response.writeHead(200);
-          response.end(html);
+            `, `<a href="/create">create</a>
+            <a href="/update?id=${queryData.id}">update</a>`);
+            response.writeHead(200);
+            response.end(html);
           });
+        });
       });
     } else if(pathName === '/update_process'){
       var body = '';
@@ -158,16 +137,14 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var title = post.title;
-          var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(error){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-              })
-            })
-
+            db.query(`
+              UPDATE topic set title = ?, description = ?, author_id = ? where id = ?`, [post.title, post.description, post.author, post.id], function(error, result){
+                if(error){
+                  throw error;
+                }
+                response.writeHead(302, {Location: `/?id=${post.id}`});
+                response.end();
+              });
           });
     }
     else if(pathName === '/delete_process'){
@@ -177,9 +154,10 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          console.log(id);
-          fs.unlink(`data/${id}`, function(error){
+          db.query(`DELETE FROM topic where id = ?`,[post.id],function(error,result){
+            if(error){
+              throw error;
+            }
             response.writeHead(302, {Location: `/`});
             response.end();
           });
